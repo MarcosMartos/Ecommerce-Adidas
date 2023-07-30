a1289d4b4fc04d9c354251ac3d27d5ac53be422a
Marcos Martos <marcosmartostuvi@gmail.com>
2023-07-30 16:49:29 -0300

    agregar ruta protegida

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "../components/layout/Layout";
 import { routes } from "./routes";
 import DashboardComponent from "../components/pages/dashboard/Dashboard";
+import ProtectedRoutes from "./ProtectedRoutes";
 
 const AppRouter = () => {
   return (
@@ -11,11 +12,14 @@ const AppRouter = () => {
           <Route key={id} path={path} element={<Element />} />
         ))}
       </Route>
-      <Route
-        key="dashboard"
-        path="/dashboard"
-        element={<DashboardComponent />}
-      />
+      <Route element={<ProtectedRoutes />}>
+        <Route
+          key="dashboard"
+          path="/dashboard"
+          element={<DashboardComponent />}
+        />
+      </Route>
+
       <Route path="*" element={<h1>404 not found</h1>} />
     </Routes>
   );
