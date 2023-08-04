4124adc5f59dd130bad0feca1980110679bfa6b4
Marcos Martos <marcosmartostuvi@gmail.com>
2023-08-04 11:53:15 -0300

    agregar links a iconos footer

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -5,6 +5,7 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -12,11 +13,35 @@ const Footer = () => {
       <Grid container>
         <Grid item className="iconosFooter">
           <ul>
-            <InstagramIcon />
-            <TwitterIcon />
-            <FacebookIcon />
-            <YouTubeIcon />
-            <GitHubIcon />
+            <Link
+              className="icono-footer"
+              to={"https://www.instagram.com/adidasar/?hl=es-la"}
+            >
+              <InstagramIcon />
+            </Link>
+            <Link
+              className="icono-footer"
+              to={
+                "https://twitter.com/adidas?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"
+              }
+            >
+              <TwitterIcon />
+            </Link>
+            <Link
+              className="icono-footer"
+              to={"https://es-la.facebook.com/adidasAR/"}
+            >
+              <FacebookIcon />
+            </Link>
+            <Link
+              className="icono-footer"
+              to={"https://www.youtube.com/user/adidas"}
+            >
+              <YouTubeIcon />
+            </Link>
+            <Link className="icono-footer" to={"https://github.com/adidas"}>
+              <GitHubIcon />
+            </Link>
           </ul>
         </Grid>
       </Grid>
